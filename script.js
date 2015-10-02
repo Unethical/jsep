6fbc6b59c29603e2efafb17eecfcb78b77f2d5ca
Unethical <sepulveda.a.jose@gmail.com>
2015-10-01 22:38:00 -0700

    Added parallax scrolling

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 var main = function(){
            $("#navBar").hide().fadeIn(300);
-           $("#header").hide().fadeIn(300);
+
+           $(function(){
+           		$("#divider").slideDown();
+           			var h = $("#divider").height();
+
+           		$(window).scroll(function(){
+           			var $DivToHide = $("#divider");
+
+           		var st = $(this).scrollTop();
+           		$DivToHide.height(h - st);
+
+
+           		}).scroll();
+
+           })
+
+           
 
 
 };
 
 $(document).keypress(function(event) {
 	if(event.which === 109) {
-		$("#logoPic").toggle(function () {
-			animate({height: "20px"}, 500);
-		});
+
+		
+		$("#logoPic").toggle();
 	}
 
 });
